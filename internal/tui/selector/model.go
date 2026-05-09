a97967a5e6f0d20ca07dc836c45948e7d25589a3
alanloffler <alanmatiasloffler@gmail.com>
2026-05-09 15:30:24 -0300

    feat(selector): model

---
+++ internal/tui/selector/model.go
@@ -0,0 +1,24 @@
+package selector
+
+import (
+	tea "charm.land/bubbletea/v2"
+	"github.com/alanloffler/bubbletea/internal/domain"
+)
+
+type DoneMsg struct {
+	Selected []domain.Project
+}
+
+type Model struct {
+	Projects []domain.Project
+	cursor   int
+	selected map[int]struct{}
+}
+
+func New(projects []domain.Project) Model {
+	return Model{Projects: projects, selected: make(map[int]struct{})}
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
