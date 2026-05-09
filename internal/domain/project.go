4c1b6124d8a217340af684fe41ff889580d6f2fd
alanloffler <alanmatiasloffler@gmail.com>
2026-05-09 16:33:48 -0300

    feat(domain): Description

--- internal/domain/project.go
+++ internal/domain/project.go
@@ -1,8 +1,9 @@
 package domain
 
 type Project struct {
-	Name       string `json:"name"`
-	Repo       string `json:"repo"`
-	InstallCmd string `json:"install_cmd"`
-	UsageHint  string `json:"usage_hint"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Repo        string `json:"repo"`
+	InstallCmd  string `json:"install_cmd"`
+	UsageHint   string `json:"usage_hint"`
 }
