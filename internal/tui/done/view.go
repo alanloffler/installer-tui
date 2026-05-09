cce0a9675b2029914fa532aab8497ee7d02ee7b3
alanloffler <alanmatiasloffler@gmail.com>
2026-05-09 17:15:39 -0300

    feat(done): home cmd

--- internal/tui/done/view.go
+++ internal/tui/done/view.go
@@ -30,7 +30,7 @@ func (m Model) View() tea.View {
 		}
 	}
 
-	s += "\n" + styles.Dim.Render("esc: inicio • q: salir")
+	s += "\n" + styles.Dim.Render("h: inicio • b/esc: volver • q: salir")
 
 	return tea.NewView(s)
 }
