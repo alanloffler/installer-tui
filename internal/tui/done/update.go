ca75becd067c317b2d5d1d0a6660e1dac76d6221
alanloffler <alanmatiasloffler@gmail.com>
2026-05-09 15:47:12 -0300

    feat(done): update

---
+++ internal/tui/done/update.go
@@ -0,0 +1,16 @@
+package done
+
+import (
+	tea "charm.land/bubbletea/v2"
+)
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	if key, ok := msg.(tea.KeyPressMsg); ok {
+		switch key.String() {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		}
+	}
+
+	return m, nil
+}
