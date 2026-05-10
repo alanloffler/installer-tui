4e4403248e307383ffe3aa6743876340e7e03436
alanloffler <alanmatiasloffler@gmail.com>
2026-05-10 15:31:06 -0300

    feat(node): menu item

--- internal/tui/node/model.go
+++ internal/tui/node/model.go
@@ -3,6 +3,7 @@ package node
 import (
 	tea "charm.land/bubbletea/v2"
 	"github.com/alanloffler/bubbletea/internal/domain"
+	"github.com/alanloffler/bubbletea/internal/tui/menu"
 )
 
 type HomeMsg struct{}
@@ -13,12 +14,16 @@ type DoneMsg struct {
 
 type Model struct {
 	Packages []domain.Package
+	items    []menu.Item
 	cursor   int
 	selected map[int]struct{}
 }
 
 func New(packages []domain.Package) Model {
-	return Model{Packages: packages, selected: make(map[int]struct{})}
+	return Model{
+		Packages: packages,
+		items:    []menu.Item{{Label: "Volver", Msg: HomeMsg{}}},
+		selected: make(map[int]struct{})}
 }
 
 func (m Model) Init() tea.Cmd {
