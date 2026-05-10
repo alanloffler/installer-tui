b584902361f86d7c1a6fe771b92f22a69eb7da9c
alanloffler <alanmatiasloffler@gmail.com>
2026-05-10 12:41:50 -0300

    feat(home): styled menu item

--- internal/tui/home/view.go
+++ internal/tui/home/view.go
@@ -8,7 +8,7 @@ import (
 )
 
 func (Model) SectionSubtitle() string {
-	return "Seleccioná tipo de instalación"
+	return "Seleccioná el tipo de instalación"
 }
 
 func (m Model) View() tea.View {
@@ -16,14 +16,16 @@ func (m Model) View() tea.View {
 
 	for i, item := range m.items {
 		cursor := " "
+
 		if m.cursor == i {
 			cursor = "▸"
+			s += fmt.Sprintf("%s %s\n", styles.SelectedStyle.Render(cursor), styles.SelectedStyle.Render(item.label))
+		} else {
+			s += fmt.Sprintf("%s %s\n", cursor, item.label)
 		}
-
-		s += fmt.Sprintf("%s %s\n", cursor, item.label)
 	}
 
-	s += "\n" + styles.Dim.Render("j/k: navegar • enter: seleccionar • q: salir")
+	s += "\n" + styles.HelpStyle.Render("j/k: navegar • enter: seleccionar • q: salir")
 
 	return tea.NewView(s)
 }
