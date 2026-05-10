d6fccfe71b552a84bca8378f49291c9f6c173c8d
alanloffler <alanmatiasloffler@gmail.com>
2026-05-10 16:22:16 -0300

    feat(done): menu item

--- internal/tui/done/model.go
+++ internal/tui/done/model.go
@@ -2,6 +2,7 @@ package done
 
 import (
 	"github.com/alanloffler/bubbletea/internal/installer"
+	"github.com/alanloffler/bubbletea/internal/tui/menu"
 
 	tea "charm.land/bubbletea/v2"
 )
@@ -14,12 +15,20 @@ type BackMsg struct {
 
 type Model struct {
 	Results []installer.Result
+	items   []menu.Item
 	cursor  int
 	back    tea.Model
 }
 
 func New(results []installer.Result, back tea.Model) Model {
-	return Model{Results: results, back: back}
+	return Model{
+		Results: results,
+		back:    back,
+		items: []menu.Item{
+			{Label: "Volver", Msg: BackMsg{Next: back}},
+			{Label: "Inicio", Msg: HomeMsg{}},
+		},
+	}
 }
 
 func (m Model) Init() tea.Cmd {
