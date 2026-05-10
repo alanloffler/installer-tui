c276d77a85771fb2910a62e45f7056e5c00075d0
alanloffler <alanmatiasloffler@gmail.com>
2026-05-10 12:30:04 -0300

    feat(main): embed json
    Embed projects & packages json files

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
 	"os"
 
@@ -9,14 +10,26 @@ import (
 	"github.com/alanloffler/bubbletea/internal/tui"
 )
 
+//go:embed configs/projects.json
+var projectsData []byte
+
+//go:embed configs/packages.json
+var packagesData []byte
+
 func main() {
-	projects, err := catalog.Load("configs/projects.json")
+	projects, err := catalog.LoadProjects(projectsData)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
-	if _, err := tea.NewProgram(tui.NewApp(projects)).Run(); err != nil {
+	packages, err := catalog.LoadPackages(packagesData)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	if _, err := tea.NewProgram(tui.NewApp(projects, packages)).Run(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
