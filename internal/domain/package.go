6d982db076b70e147869c1fefc6e10585427ae1f
alanloffler <alanmatiasloffler@gmail.com>
2026-05-10 16:59:40 -0300

    feat(package): cmd & description

--- internal/domain/package.go
+++ internal/domain/package.go
@@ -1,7 +1,9 @@
 package domain
 
 type Package struct {
-	Name       string `json:"name"`
-	Repo       string `json:"repo"`
-	InstallCmd string `json:"install_cmd"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Cmd         string `json:"cmd"`
+	Repo        string `json:"repo"`
+	InstallCmd  string `json:"install_cmd"`
 }
