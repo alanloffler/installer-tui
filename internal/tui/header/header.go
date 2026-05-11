828a57ba73969c54408493f6226172bebc66a5e1
alanloffler <alanmatiasloffler@gmail.com>
2026-05-11 11:09:58 -0300

    feat(header): data & styles

--- internal/tui/header/header.go
+++ internal/tui/header/header.go
@@ -1,22 +1,37 @@
 package header
 
 import (
+	"os"
+	"runtime"
 	"strings"
 
+	"charm.land/lipgloss/v2"
 	"github.com/alanloffler/bubbletea/internal/tui/styles"
 )
 
 var (
-	titleStyle    = styles.HeadingStyle.Padding(0, 2)
-	versionStyle  = styles.SubtextStyle
-	subtitleStyle = styles.SubtextStyle.Bold(true).Italic(true).PaddingLeft(2)
+	titleStyle = styles.HeadingStyle.Padding(0, 2, 0, 2).Border(lipgloss.Border{
+		TopLeft: "┌", Top: "─", TopRight: "┐",
+		Right:      "│",
+		BottomLeft: "└", Bottom: "─", BottomRight: "┘",
+		Left: "│"},
+	).BorderForeground(styles.ColorMauve)
+	subtitleStyle = styles.SubtextStyle.Bold(true).Italic(true)
 )
 
 func Render(appTitle, appSubtitle, sectionSubtitle string) string {
 	var b strings.Builder
 	b.WriteString(titleStyle.Render(appTitle))
-	b.WriteString(versionStyle.Render(appSubtitle))
-	b.WriteString("\n\n")
+	b.WriteString("\n")
+
+	b.WriteString(styles.PrimaryStyle.Render("Version: "))
+	b.WriteString(styles.SubtextStyle.Render(appSubtitle) + "\n")
+	b.WriteString(styles.PrimaryStyle.Render("OS: "))
+	b.WriteString(styles.SubtextStyle.Render(runtime.GOOS) + "\n")
+
+	dir, _ := os.Getwd()
+	b.WriteString(styles.PrimaryStyle.Render("Path: "))
+	b.WriteString(styles.SubtextStyle.Render(dir) + "\n\n")
 
 	if sectionSubtitle != "" {
 		b.WriteString(subtitleStyle.Render(sectionSubtitle))
